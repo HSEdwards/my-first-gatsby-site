aa63e8e54e3977a638798a1f8753d1bd894c42fd
Hannah Edwards <hsedwards@twc.com>
2022-03-02 01:51:58 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
+  
     siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
+      title: "Films and Stuff",
     },
+    
     plugins: [
       "gatsby-plugin-image",
       "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
