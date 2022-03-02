3c5ab9c2abc566504ae269c19f27b8670934aedf
Hannah Edwards <hsedwards@twc.com>
2022-03-01 21:53:01 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.
+          I also really like to juggle and I would ideally be running away to the circus.
+          If only I had the gas money.
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
