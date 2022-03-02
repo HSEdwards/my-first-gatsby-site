c2851c0df2248bd66f7d3e61d74b0e0baeb0b172
Hannah Edwards <hsedwards@twc.com>
2022-03-02 00:01:33 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,9 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.
-          I also really like to juggle and I would ideally be running away to the circus.
-          If only I had the gas money.
+      <p>
+        Hiya! I'm a dork with some mild coding skills and a very high interest in tv and film. I've decided
+        to make the best of both worlds and code a reccomendations list for anyone who has some time to kill.
       </p>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>This is a webpage dedicated to some of the shows and movies that have brough me some joy.</p>
+      <StaticImage
+        alt="A water color picture of a deep blue police box flying in space. It is known as the TARDIS"
+        //src="https://i.pinimg.com/originals/01/26/5a/01265af8dc818dd538cb50cac2f7d92c.jpg"
+        src="../images/tardis_falling_lights.jpg"
+        />
     </Layout>
   )
 }
